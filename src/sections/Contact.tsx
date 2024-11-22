fc6b71de14d21c360345534a64af431ea5bc6678
solo-mizan <mizanbd.eee@gmail.com>
2024-11-23 02:56:43 +0600

    fix: small bugs

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -13,10 +13,10 @@ export const ContactSection = () => {
           <div className="flex md:flex-row flex-col items-center gap-8 md:gap-16">
             <div>
               <h2 className="font-serif text-2xl md:text-3xl">
-                Let's create something amazing together
+                Let&apos;s create something amazing together
               </h2>
               <p className="mt-2 text-sm md:text-base">
-                Ready to bring your next project to life? Let's connect and
+                Ready to bring your next project to life? Let&apos;s connect and
                 discuss how I can help you achieve your goals.
               </p>
             </div>
